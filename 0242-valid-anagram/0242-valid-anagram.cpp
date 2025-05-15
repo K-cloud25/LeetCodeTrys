caa10cb3c498d0c9976539f73aec4a5af9976b6a
Keyur Oka <78078076+K-cloud25@users.noreply.github.com>
2025-05-15 17:13:42 +0530

    Time: 0 ms (100%), Space: 9.7 MB (74.21%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int count[26] = {0};
-        
-        // Count the frequency of characters in string s
-        for (char x : s) {
-            count[x - 'a']++;
-        }
-        
-        // Decrement the frequency of characters in string t
-        for (char x : t) {
-            count[x - 'a']--;
-        }
-        
-        // Check if any character has non-zero frequency
-        for (int val : count) {
-            if (val != 0) {
+        int map[26] = {0};
+        for(char c : s)
+            map[c-'a']++;
+        for(char c : t)
+            map[c-'a']--;
+
+        for(int i=0;i<26;i++){
+            if ( map[i] !=0 )
                 return false;
-            }
         }
-        
         return true;
     }
 };
