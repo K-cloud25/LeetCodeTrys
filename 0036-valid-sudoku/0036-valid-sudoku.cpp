8260937bb11f4ad171a3de0df8d5bef7cb579dc5
Keyur Oka <78078076+K-cloud25@users.noreply.github.com>
2025-05-19 19:27:24 +0530

    Time: 0 ms (100%), Space: 25.8 MB (12.82%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -4,22 +4,22 @@ public:
         unordered_set<char> r[9];
         unordered_set<char> c[9];
         unordered_set<char> b[9];
-        for(int i =0;i<9;i++){
-            for(int j=0;j<9;j++){
-                
-                if ( board[i][j] == '.' ) continue;
-                char val = board[i][j];
-                int boxVal = (i / 3) * 3 + ( j / 3 );
 
-                if ( r[i].count(val) || c[j].count(val) || b[boxVal].count(val) ){
-                    return false;
-                }
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if( board[i][j] == '.' ) continue;
+            
+                int val = board[i][j];
+                int boxVal = ( i / 3) * 3 + ( j / 3 );
+
+                if ( r[i].count(val) || c[j].count(val) || b[boxVal].count(val) )   return false;
 
                 r[i].insert(val);
                 c[j].insert(val);
                 b[boxVal].insert(val);
             }
         }
+
         return true;
     }
 };
