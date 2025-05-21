28392e8023c03069d22106e4b9691cd27c7eaac8
Keyur Oka <78078076+K-cloud25@users.noreply.github.com>
2025-05-21 20:42:24 +0530

    Time: 23 ms (57.13%), Space: 17.6 MB (31%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,49 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-vector<vector<int>> th;
+        vector<vector<int>> res;
         sort(nums.begin(), nums.end());
         int n = nums.size();
+        for(int i=0;i<n;i++){
+            if ( i>0 && nums[i]==nums[i-1]) continue;
 
-        for(int i = 0; i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if (j>i+1 && nums[j]==nums[j-1]) continue;
 
-            if ( i > 0 && nums[i] == nums[i-1]){
-                continue;
-            }
+                int k=j+1;
+                int l=n-1;
 
-            for(int l = i+1; l<n; l++){
+                while( k<l ){
+                    long long int total = nums[i];
+                    total+=nums[j];
+                    total+=nums[k];
+                    total+=nums[l];
 
-                if ( l > i+1 && nums[l] == nums[l-1]){
-                    continue;
-                }
-
-                int j = l+1;
-                int k = n - 1;
-
-                while( j < k ){
-
-                    long long total = nums[i];
-                    total +=nums[j];
-                    total += nums[k];
-                    total += nums[l];
-
-                    if ( total > target ){
-                        k-=1;
-                    }else if(total < target){
-                        j+=1;
+                    if( total < target){
+                        k++;
+                    }else if ( total > target){
+                        l--;
                     }else{
-                        th.push_back({nums[i], nums[j], nums[k], nums[l]});
-                        j+=1;
-                        while(nums[j] == nums[j-1] && j < k){
-                            j+=1;
-                        }
+                        res.push_back({nums[i], nums[j], nums[k], nums[l]});
+                        k++;
+                        while ( k<l && nums[k]==nums[k-1] ) k++;
                     }
-
                 }
-
             }
         }
 
-        return th;
+        return res;
     }
 };
