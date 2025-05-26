4e012670b0d05dfceb610b04ac64476589226413
Keyur Oka <78078076+K-cloud25@users.noreply.github.com>
2025-05-26 19:35:06 +0530

    Time: 0 ms (100%), Space: 10.8 MB (71.84%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int l=0,res=0, r=0,f=0;
-        vector<int> mp(26,0);
-
+        int l=0,r=0,f=0,res=0;
+        vector<int> arr(26,0);
         while( r<s.length() ){
-            mp[s[r]-'A']++;
-            f=max(f, mp[s[r]-'A']);
-            if((r-l+1)-f>k){
-                mp[s[l]-'A']--;
-                f=0;
+            arr[s[r] - 'A']++;
+            f=max( f, arr[s[r]-'A'] );
+            while( ( r-l+1 )-f>k ){
+                arr[s[l]-'A']--;
                 l++;
             }
-            res=max(res,(r-l+1));
+            res=max((r-l+1), res);
             r++;
         }
-
         return res;
     }
 };
