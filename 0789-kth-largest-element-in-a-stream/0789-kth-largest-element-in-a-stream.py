c8dad4b6931efff5c9718abcc5a84d1e9b781f21
Keyur Oka <78078076+K-cloud25@users.noreply.github.com>
2025-11-08 10:36:51 +0530

    Time: 20 ms (40.1%), Space: 23.6 MB (83.55%) - LeetHub

--- 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.py
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.py
@@ -1,30 +1,17 @@
 class KthLargest:
-
     def __init__(self, k: int, nums: List[int]):
+        heapify(nums)
+        self.heap = nums
         self.k = k
-        self.stream = nums
-        self.stream.sort()
+        while len(self.heap) > self.k:
+            heappop(self.heap)
 
     def add(self, val: int) -> int:
-        index = self.getIndex(val)
-        # Add val to correct position
-        self.stream.insert(index, val)
-        return self.stream[-self.k]
+        heappush(self.heap, val)
+        if len(self.heap) > self.k:
+            heappop(self.heap)
+        return self.heap[0]
 
-    def getIndex(self, val: int) -> int:
-        left, right = 0, len(self.stream) - 1
-        while left <= right:
-            mid = (left + right) // 2
-            mid_element = self.stream[mid]
-            if mid_element == val:
-                return mid
-            # Go to left half
-            elif mid_element > val:
-                right = mid - 1
-            # Go to right half
-            else:
-                left = mid + 1
-        return left
         
 
 
