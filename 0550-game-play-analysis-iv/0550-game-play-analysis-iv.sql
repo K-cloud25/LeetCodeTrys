7278e135bac7dc9ee85a97b6584a55d123467b74
Keyur Oka <78078076+K-cloud25@users.noreply.github.com>
2026-01-12 19:46:02 +0530

    Time: 509 ms (77.72%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,23 +1,11 @@
-WITH FirstLogin AS (
-    SELECT
-        player_id,
-        MIN(event_date) AS first_login
-    FROM Activity
-    GROUP BY player_id
-),
-ConsecutiveLogin AS (
-    SELECT
-        a.player_id
-    FROM Activity a
-    JOIN FirstLogin fl
-    ON a.player_id = fl.player_id
-    AND a.event_date = DATE_ADD(fl.first_login, INTERVAL 1 DAY)
+WITH a AS (
+    SELECT player_id, MIN(event_date) AS event_date 
+    FROM Activity 
+    GROUP BY player_id 
 )
-SELECT
-    ROUND(
-        COUNT(DISTINCT cl.player_id) * 1.0 / COUNT(DISTINCT fl.player_id),
-        2
-    ) AS fraction
-FROM FirstLogin fl
-LEFT JOIN ConsecutiveLogin cl
-ON fl.player_id = cl.player_id;
+SELECT ROUND(COUNT(b.event_date)/ COUNT(*), 2) AS fraction 
+FROM Activity b
+RIGHT JOIN a
+ON b.player_id = a.player_id 
+AND 
+DATE_SUB(b.event_date, INTERVAL 1 DAY) = a.event_date
