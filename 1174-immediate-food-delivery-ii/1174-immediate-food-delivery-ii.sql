dfa2acef5c5455ea79161684ae03cdb7d90e3f4d
Keyur Oka <78078076+K-cloud25@users.noreply.github.com>
2026-01-15 11:58:00 +0530

    Time: 608 ms (45.68%), Space: 0B (100%) - LeetHub

---
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -0,0 +1,10 @@
+SELECT ROUND((SUM( 
+    CASE WHEN DATEDIFF(order_date, customer_pref_delivery_date) = 0
+    THEN 1
+    ELSE 0
+    END
+ ) / COUNT(DISTINCT customer_id))*100, 2 )AS immediate_percentage
+FROM Delivery
+WHERE (customer_id, order_date) IN (    SELECT customer_id, MIN(order_date)
+    FROM Delivery
+    GROUP BY customer_id)
