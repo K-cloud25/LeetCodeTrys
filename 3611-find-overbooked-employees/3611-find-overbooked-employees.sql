0a0987d468e60b61a87ba7511a03019360dd88d9
Keyur Oka <78078076+K-cloud25@users.noreply.github.com>
2026-01-27 20:08:35 +0530

    Time: 474 ms (84.97%), Space: 0B (100%) - LeetHub

--- 3611-find-overbooked-employees/3611-find-overbooked-employees.sql
+++ 3611-find-overbooked-employees/3611-find-overbooked-employees.sql
@@ -1,11 +1,24 @@
-WITH process_1 AS (
-SELECT employee_id, SUM(duration_hours) duration_total
-FROM meetings
-GROUP BY employee_id, WEEKOFYEAR(meeting_date), YEAR(meeting_date)
+with week_filter as (
+    select*, sum(duration_hours) as t
+    from (
+        select *, WEEKOFYEAR(meeting_date)as week_year,
+        year(meeting_date) as Y
+        from meetings 
+    ) as sub
+    group by employee_id, week_year, Y
+    having sum(duration_hours) > 20
+),
+
+meeting_heavy_filter as 
+(
+    select employee_id, count(employee_id) as meeting_heavy_weeks
+    from week_filter
+    group by employee_id
+    having count(employee_id) > 1
 )
-SELECT p.employee_id, e.employee_name, e.department, COUNT(p.employee_id) meeting_heavy_weeks 
-FROM process_1 p INNER JOIN employees e ON p.employee_id = e.employee_id
-WHERE duration_total > 20
-GROUP BY p.employee_id, e.employee_name, e.department
-HAVING COUNT(p.employee_id) > 1
-ORDER BY meeting_heavy_weeks DESC, employee_name;
+
+select a.employee_id, employee_name, department, meeting_heavy_weeks
+from meeting_heavy_filter as a
+join employees as b
+    on a.employee_id = b.employee_id
+order by meeting_heavy_weeks desc, employee_name asc
