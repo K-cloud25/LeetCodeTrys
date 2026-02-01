2f77ae2a1f1ea9047a94fe71bcd044f6793240e7
Keyur Oka <78078076+K-cloud25@users.noreply.github.com>
2026-02-01 18:35:29 +0530

    Time: 0 ms (100%), Space: 12 MB (55.49%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
@@ -1,29 +1,34 @@
 class Solution {
-    vector<bool> used;
-    int target;
-
 public:
-    bool canPartitionKSubsets(vector<int>& nums, int k) {
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-        if (sum % k != 0) return false;
+    bool helper(vector<int>& nums , vector<int>& groups , int target , int idx){
+        if(idx == nums.size()) return true;
+        for(int i = 1 ; i<groups.size() ; i++){
+            
+            if(i > 1 && groups[i-1] == groups[i]) continue;
 
-        target = sum / k;
-        sort(nums.rbegin(), nums.rend());
-        used.assign(nums.size(), false);
-        return backtrack(nums, k, 0, 0);
-    }
+            if(nums[idx] + groups[i] > target)continue;
+            groups[i] += nums[idx];
+            if(helper(nums , groups , target , idx + 1)) return true;
+            groups[i] -= nums[idx];
 
-private:
-    bool backtrack(vector<int>& nums, int k, int currentSum, int start) {
-        if (k == 0) return true;
-        if (currentSum == target) return backtrack(nums, k - 1, 0, 0);
-
-        for (int i = start; i < nums.size(); i++) {
-            if (used[i] || currentSum + nums[i] > target) continue;
-            used[i] = true;
-            if (backtrack(nums, k, currentSum + nums[i], i + 1)) return true;
-            used[i] = false;
+            if(nums[i] == 0)break;
         }
         return false;
+    }
+
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        if(k == 1)return true;
+        if(k > nums.size()) return false;
+
+        sort(nums.begin() , nums.end());
+        reverse(nums.begin() , nums.end());
+        vector<int> groups(k+1 , 0);
+
+        int target = 0;
+        for(const auto val : nums)target += val;
+        if(target % k || nums[0] > target)return false;
+
+        target /= k;
+        return helper(nums , groups , target , 0);
     }
 };
