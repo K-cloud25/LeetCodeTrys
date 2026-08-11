5691733e00787cc729e1e0dbdac0edaf2b1a6120
Keyur Oka <78078076+K-cloud25@users.noreply.github.com>
2026-08-11 21:21:19 +0530

    Time: 1 ms (94.77%), Space: 23.3 MB (99.73%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,17 +3,16 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
         int k = 0;
-        for(int i=0;i<intervals.size();i++){
-            if ( intervals[k][1] >= intervals[i][0]){
-                intervals[k][1] = max(intervals[k][1], intervals[i][1]);
+        for(int i=0; i<intervals.size(); i++){
+            if ( intervals[k][1] >= intervals[i][0] ){
+                intervals[k][1] = max(intervals[i][1], intervals[k][1]);
             }else{
                 k++;
-                intervals[k] = intervals[i];
+                intervals[k] = intervals[i]; 
             }
         }
 
         intervals.resize(k+1);
         return intervals;
-
     }
 };
