74099ed03c924cfa551bc6cc8df74bb3805f4fe4
Keyur Oka <78078076+K-cloud25@users.noreply.github.com>
2026-09-13 10:42:44 +0530

    Time: 0 ms (100%), Space: 17.4 MB (88.61%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> ans = {-1,-1};
+        int left = -1, right = -1;
+        // left 
+        int low = 0, high = nums.size()-1;
+        int t = -1;
+        while(low<=high){
+            int mid = low + ( high - low ) /2;
+            if ( nums[mid] == target ) {
+                t = mid;
+                high = mid - 1;
+            } else if ( nums[mid] > target ) high = mid - 1;
+            else low = mid + 1; 
+        }
 
-        int left = 0;
-        int right = nums.size()-1;
-
-        while(left<=right){
-            int mid = left + (right-left)/2;
-            if ( nums[mid] <= target){
-                if ( nums[mid] == target)
-                    ans[1] = mid;
-                left=mid+1;
+        left=t;
+        low = 0, high = nums.size() -1;
+        t=-1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if ( nums[mid] == target ){
+                t = mid;
+                low = mid + 1;
             }
-            if ( nums[mid] > target) right = mid-1;
+            else if ( nums[mid] < target) low = mid + 1;
+            else high = mid - 1;
         }
-        left=0;right=nums.size()-1;
+        right = t;
 
-        while(left<=right){
-            int mid = left + (right - left)/2;
-            if ( nums[mid] < target) left = mid + 1;
-            if ( nums[mid] >= target ){
-                if ( nums[mid] == target) 
-                    ans[0] = mid;
-                right=mid-1;
-            } 
-        }
-
-        return ans;
+        return {left, right};
     }
 };
